ecfe8aafe1daad54b77381d23949d5cbf720882c
HTBBM <bruno.boldrinp90@gmail.com>
2024-06-24 11:19:50 -0300

    add user input

---
+++ src/components/LoginForm.js
@@ -0,0 +1,12 @@
+
+import styles from './LoginForm.module.css'
+
+export default function LoginForm(){
+    return(
+        <div>
+            <label htmlFor="user">User</label>
+            <input type="text" name="user"/>
+
+        </div>
+    )
+}

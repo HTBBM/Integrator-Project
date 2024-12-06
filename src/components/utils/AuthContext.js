13badcfb3384a2bcca68824e34012c2c214a9bcc
Bruno Boldrin Mathias <136337818+HTBBM@users.noreply.github.com>
2024-12-06 09:40:21 -0300

    Enable Authentication

--- src/components/utils/AuthContext.js
+++ src/components/utils/AuthContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({ token: true });
+  const [auth, setAuth] = useState({ token: false });
 
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
